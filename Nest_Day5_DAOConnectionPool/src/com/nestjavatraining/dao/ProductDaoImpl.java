31499f10a4f0ab04a96c687e07339c5fa7714617
Amarnath <88896502+j1999amar@users.noreply.github.com>
2023-02-17 10:39:22 +0530

    Day 5

---
+++ Nest_Day5_DAOConnectionPool/src/com/nestjavatraining/dao/ProductDaoImpl.java
@@ -0,0 +1,33 @@
+package com.nestjavatraining.dao;
+
+import java.util.List;
+
+import com.nestjavatraining.entity.Product;
+
+public class ProductDaoImpl implements ProductDao {
+
+	@Override
+	public void saveProduct(Product product) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Product> listAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProduct(String productCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteProduct(String productCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Nest_Day5_DAOConnectionPool/src/com/nestjavatraining/service/ProductServiceImpl.java
@@ -0,0 +1,33 @@
+package com.nestjavatraining.service;
+
+import java.util.List;
+
+import com.nestjavatraining.entity.Product;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public void saveProduct(Product product) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Product> listAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProduct(String productCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteProduct(String productCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
